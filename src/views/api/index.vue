9021e85cffd848b3ee68940bedfc897d100894fc
苏震 <suzhen@viewhigh.com>
2019-05-26 00:45:04 +0800

    5-25

--- src/views/api/index.vue
+++ src/views/api/index.vue
@@ -1,8 +1,10 @@
 <template>
-<div style="text-align:center;">
-  <!-- <el-row>{{userList}}</el-row> -->
-  <el-row><el-button @click="handleTestAPI">测试接口</el-button></el-row>
-</div>
+  <div style="text-align:center;">
+    <!-- <el-row>{{userList}}</el-row> -->
+    <el-row>
+      <el-button @click="handleTestAPI">测试接口</el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -10,18 +12,24 @@ import { createNamespacedHelpers, mapGetters } from "vuex";
 const { mapActions: userAction } = createNamespacedHelpers("user");
 
 export default {
-  computed:{
+  computed: {
     ...mapGetters(["userList"]),
   },
   methods: {
-    ...userAction(["getUserList"]),
+    // ...userAction(["getUserList"]),
     handleTestAPI() {
-      this.getUserList().then(res => {
+      this.$store.dispatch('user/getUserList').then(res => {
         this.$message({
           type: "success",
           message: res
         });
-      });
+      })
+      // this.getUserList().then(res => {
+      //   this.$message({
+      //     type: "success",
+      //     message: res
+      //   });
+      // });
     }
   }
 };
